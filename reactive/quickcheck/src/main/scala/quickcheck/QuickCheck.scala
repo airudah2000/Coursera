314eb1610d2856aa2c62d3c2bbde35250b9f427c
Robert Udah <robert.udah@sucfin.com>
2015-04-20 17:46:10 +0100

    Implimentation of solution

--- reactive/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ reactive/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -13,14 +13,63 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     val h = insert(a, empty)
     findMin(h) == a
   }
-//
-//  property("deleteMin1") = forAll { h: H =>
-//    val resultantHeap = if(isEmpty(h)) empty else deleteMin(h)
-//    !h.equals(resultantHeap)
-//  }
 
-  lazy val genHeap: Gen[H] = ???
+  lazy val generateFullHeap: Gen[H] = for {
+    a <- arbitrary[Int]
+    h <- genHeap
+  } yield insert(a, h)
+
+  lazy val genHeap: Gen[H] = oneOf(generateFullHeap, value(empty))
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
+
+  property("gen1") = forAll { h: H =>
+    val m = if (isEmpty(h)) 0 else findMin(h)
+    findMin(insert(m, h)) == m
+  }
+
+  property("insTwoElemInEmpty") = forAll { (a1: Int, a2: Int) =>
+    val h = insert(a2, insert(a1, empty))
+    val smallest = if (a1 < a2) a1 else a2
+    findMin(h) == smallest
+  }
+
+ property("insDelMin") = forAll { a: Int =>
+    val h = insert(a, empty)
+    val h1 = deleteMin(h)
+    h1 == empty
+  }
+
+  property("meldMin") = forAll { (h1: H, h2: H) =>
+    val min1 = findMin(h1)
+    val min2 = findMin(h2)
+    val m = meld(h1, h2)
+    val minMeld = findMin(m)
+    minMeld == min1 || minMeld == min2
+  }
+
+  // Given any heap, you should get a sorted sequence of elements when continually finding and deleting minima. (Hint: recursion and helper functions are your friends.)
+  property("remMin") = forAll { h: H =>
+    def remMin(ts: H, as: List[Int]): List[Int] = {
+      if (isEmpty(ts)) as
+      else findMin(ts) :: remMin(deleteMin(ts), as)
+    }
+    val xs = remMin(h, Nil)
+    xs == xs.sorted
+  }
+
+  // Take two arbitrary heaps, meld together. Then remove min from 1 and insert into 2, meld the results. Compare two melds by comparing sequences of ranks.
+  property("meldMinMove") = forAll { (h1: H, h2: H) =>
+    def remMin(ts: H, as: List[Int]): List[Int] = {
+      if (isEmpty(ts)) as
+      else findMin(ts) :: remMin(deleteMin(ts), as)
+    }
+    val meld1 = meld(h1, h2)
+    val min1 = findMin(h1)
+    val meld2 = meld(deleteMin(h1), insert(min1, h2))
+    val xs1 = remMin(meld1, Nil)
+    val xs2 = remMin(meld2, Nil)
+    xs1 == xs2
+  }
 
 }
