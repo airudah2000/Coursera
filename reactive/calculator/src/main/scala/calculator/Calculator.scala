2b2b42be6b88bd1602fb9d6b90e95d06ab4addea
Robert Udah <robert.udah@sucfin.com>
2015-04-29 18:09:46 +0100

    Attetmpts at extending the assignment

--- reactive/calculator/src/main/scala/calculator/Calculator.scala
+++ reactive/calculator/src/main/scala/calculator/Calculator.scala
@@ -1,5 +1,7 @@
 package calculator
 
+import scala.collection.immutable.Iterable
+
 sealed abstract class Expr
 final case class Literal(v: Double) extends Expr
 final case class Ref(name: String) extends Expr
@@ -11,17 +13,30 @@ final case class Divide(a: Expr, b: Expr) extends Expr
 object Calculator {
 
   def computeValues(namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
+    val value =
+      for {
+        (k, v) <- namedExpressions
+        expression: Expr = getReferenceExpr(k, namedExpressions)
+        value: Double = eval(expression, namedExpressions)
+      }
+        yield (k, Signal(value))
+    value.toMap
   }
 
-  def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
+  def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = expr match {
+    case Literal(value : Double) => value
+    case Ref(name: String) => Double.NaN
+    case Plus(a: Expr, b: Expr) => Double.NaN
+    case Minus(a: Expr, b: Expr) => Double.NaN
+    case Times(a: Expr, b: Expr) => Double.NaN
+    case Divide(a: Expr, b: Expr) => Double.NaN
+    case _ => Double.NaN
   }
 
   /** Get the Expr for a referenced variables.
-   *  If the variable is not known, returns a literal NaN.
-   */
-  private def getReferenceExpr(name: String, references: Map[String, Signal[Expr]]) = {
+    * If the variable is not known, returns a literal NaN.
+    */
+  private def getReferenceExpr(name: String, references: Map[String, Signal[Expr]]): Expr = {
     references.get(name).fold[Expr] {
       Literal(Double.NaN)
     } { exprSignal =>
--- reactive/calculator/src/test/scala/calculator/CalculatorSuite.scala
+++ reactive/calculator/src/test/scala/calculator/CalculatorSuite.scala
@@ -1,13 +1,13 @@
 package calculator
 
+import calculator.{Literal, Signal}
 import org.scalatest.FunSuite
-
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
-
 import org.scalatest._
-
 import TweetLength.MaxTweetLength
+import Polynomial._
+import Calculator._
 
 @RunWith(classOf[JUnitRunner])
 class CalculatorSuite extends FunSuite with ShouldMatchers {
@@ -20,7 +20,7 @@ class CalculatorSuite extends FunSuite with ShouldMatchers {
     text.codePointCount(0, text.length)
 
   test("tweetRemainingCharsCount with a constant signal") {
-    val result = TweetLength.tweetRemainingCharsCount(Var("hello world"))
+    val result = calculator. TweetLength.tweetRemainingCharsCount(Var("hello world"))
     assert(result() == MaxTweetLength - tweetLength("hello world"))
 
     val tooLong = "foo" * 200
@@ -35,6 +35,7 @@ class CalculatorSuite extends FunSuite with ShouldMatchers {
 
 
   test("colorForRemainingCharsCount with a constant signal") {
+
     val resultGreen1 = TweetLength.colorForRemainingCharsCount(Var(52))
     assert(resultGreen1() == "green")
     val resultGreen2 = TweetLength.colorForRemainingCharsCount(Var(15))
@@ -49,6 +50,49 @@ class CalculatorSuite extends FunSuite with ShouldMatchers {
     assert(resultRed1() == "red")
     val resultRed2 = TweetLength.colorForRemainingCharsCount(Var(-5))
     assert(resultRed2() == "red")
+  }
+
+  test("polynomial delta") {
+    val deltaValue1 = computeDelta(Signal(2.0), Signal(5.0), Signal(3.0))
+    assert(deltaValue1.apply === 1.0)
+
+    val deltaValue2 = computeDelta(Signal(4.0), Signal(2.0), Signal(1.0))
+    assert(deltaValue2.apply === -12.0)
+  }
+
+  test("polynomial solution") {
+    val deltaValue1 = computeDelta(Signal(2.0), Signal(5.0), Signal(3.0))
+    val solution1 = computeSolutions(Signal(2.0), Signal(5.0), Signal(3.0), deltaValue1)
+    assert(solution1.apply === Set(-1.0, -1.5))
+
+    val deltaValue2 = computeDelta(Signal(4.0), Signal(2.0), Signal(1.0))
+    val solution2 = computeSolutions(Signal(4.0), Signal(2.0), Signal(1.0), deltaValue2)
+    assert(solution2.apply === Set())
+  }
+
+  test("Calculate expression literal") {
+//    abstract class Expr
+//    case class Literal(v: Double) extends Expr
+//
+//    def getReferenceExpr(name: String, references: Map[String, Signal[Expr]]): Expr = {
+//      references.get(name).fold[Expr] {
+//        Literal(Double.NaN)
+//      } { exprSignal =>
+//        exprSignal()
+//      }
+//    }
+//
+//    val namedExpressionsA: Map[String, Nothing] = (List("a", "b") zip List(Signal(Literal(2)), Signal(Literal(3)))).toMap
+//    val (key1, val1) = namedExpressions.head
+//    val exp1 = getReferenceExpr(key1, namedExpressions)
+//
+//    val (key2, val2) = namedExpressions.tail
+//    val exp2 = getReferenceExpr(key2, namedExpressions)
+//
+//    val namedExpressionsB: Map[String, Nothing] = (List("a", "b") zip List(Signal(exp1), Signal(exp2))).toMap
+//
+//    val computedValues1 = computeValues(Map(key1, Signal(namedExpressions)))
+//    assert(computedValues1 === 1, s"values were $computedValues")
   }
 
 }
